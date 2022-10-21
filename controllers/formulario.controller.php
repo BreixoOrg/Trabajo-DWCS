aabc395a7d38e068980b1bb37ccbf71bb605d502
BreixoOrg <nikiraixd@gmail.com>
2022-10-21 17:57:59 +0200

    Todo listo para empezar

--- controllers/formulario.controller.php
+++ controllers/formulario.controller.php
@@ -3,5 +3,5 @@ $data['titulo'] = "Plantilla formulario";
 $data["div_titulo"] = "Formulario";
 
 include 'views/templates/header.php';
-include 'views/formulario.view.php';
+include 'views/.view.php';
 include 'views/templates/footer.php';
