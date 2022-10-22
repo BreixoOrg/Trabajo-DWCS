ac383bbe150fc2301dd7f6895b4294c438bf8973
BreixoOrg <nikiraixd@gmail.com>
2022-10-22 13:14:36 +0200

    Sanitizar y checkForm listos

--- controllers/calculoNotas.Breixo.controller.php
+++ controllers/calculoNotas.Breixo.controller.php
@@ -3,7 +3,77 @@
 $data = [];
 
 $data['titulo'] = "Trabajo-DWCS";
-$data["div_titulo"] = "Cálculo de Notas";
+$data['div_titulo'] = "Cálculo de Notas";
+
+if(isset($_POST['Enviar'])){
+    var_dump($_POST);
+    $data['input'] = sanitizarInput($_POST);
+    $data['errores'] = checkForm($_POST);
+    
+    if(count($data['errores']) == 0){
+        
+    }
+}
+
+function sanitizarInput($post){
+    return filter_var_array($post,FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+function checkForm($post){
+    
+    $errores = [];
+    
+    if(empty($post['textAreaU'])){
+        $errores['errores'] = "Este campo no puede estar vacío";
+    }
+    else{
+        $json = json_decode($post['textAreaU'], true);
+        if(json_last_error() !== JSON_ERROR_NONE){
+            $errores['errores'] = 'El formato del JSON no es correcto';
+        }
+        else{
+            $err_msg = "";
+            
+            foreach ($errores as $asignatura => $clase) {
+                
+                if(empty($asignatura)){
+                    $err_msg .= "El nombre de la asignatura no puede estar vacío<br />";
+                }
+                
+                if(!is_array($clase)){
+                    $err_msg .= "El módulo '".htmlentities($clase)."' no tiene un array de alumnos<br />"; 
+                }
+                else{
+                    foreach($clase as $alumno => $notas){
+                        if(empty($alumno)){
+                            $err_msg .= "El nombre de un alumno/a no puede estar vacío<br />";
+                        }
+                        if(!is_array($notas)){
+                            $err_msg .= "El/La alumno/a ".htmlentities($alumno)." no tiene un array de notas asociado<br />"; 
+                        }
+                        else{
+                            foreach ($notas as $nota) {
+                                if(!is_numeric($nota)){
+                                    $err_msg .= "En la asignatura de ".htmlentities($asignatura).", ". ucfirst(htmlentities($alumno)." tiene una nota no numérica<br />");
+                                }
+                                else{
+                                    if($nota < 0 || $nota > 10){
+                                        $err_msg .= "En la asignatura de ".htmlentities($asignatura).", ". ucfirst(htmlentities($alumno)." tiene una nota superior a 10 o inferior a 0<br />");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            $errores['errores'] = $err_msg;
+        }
+    }
+    
+    
+    return $errores;
+}
+
 
 include 'views/templates/header.php';
 include 'views/calculoNotas.Breixo.view.php';
--- views/calculoNotas.Breixo.view.php
+++ views/calculoNotas.Breixo.view.php
@@ -17,62 +17,13 @@
             <!-- Card Body -->
             <div class="card-body">
                 <!--<form action="./?sec=formulario" method="post">                   -->
-                <form method="get">
-                    <input type="hidden" name="sec" value="formulario" />
+                <form method="post" action="./?sec=calculoNotas.Breixo">
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1">Enderezo electrónico</label>
-                        <input class="form-control" id="exampleFormControlInput1" type="email" name="email" placeholder="name@example.com">
+                        <label for="textAreaU">Introduzca su JSON</label>
+                        <textarea class="form-control" id="textAreaU" name="textAreaU" rows="10" placeholder=""></textarea>
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlSelect1">Examplo select</label>
-                        <select class="form-control" id="exampleFormControlSelect1" name="selectnormal">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlSelect2">Exemplo multiple select</label>
-                        <select class="form-control" id="exampleFormControlSelect2" multiple="" name="selectmultiple[]">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlSelect2">Exemplo checkbox</label>  
-
-                            <div class="row">
-                            <div class="col-sm">
-                            <div class="form-check">
-                                <input class="form-check-input" id="flexCheckDefault" type="checkbox" value="a" name="opcions[]">
-                                <label class="form-check-label" for="flexCheckDefault">Default checkbox</label>
-                            </div>
-                            </div>
-                            <div class="col-sm">
-                            <div class="form-check">
-                                <input class="form-check-input" id="flexCheckChecked" type="checkbox" value="b" checked" name="opcions[]">
-                                <label class="form-check-label" for="flexCheckChecked">Checked checkbox</label>
-                            </div>
-                            </div>
-                        <div class="col-sm">
-                            <div class="form-check">
-                                <input class="form-check-input" id="flexCheckDisabled" type="checkbox" value="c" disabled name="opcions[]">
-                                <label class="form-check-label" for="flexCheckDisabled">Disabled checkbox</label>
-                            </div>
-                        </div>
-                            </div>
-                        </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlTextarea1">Exemplo textarea</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" name="textarea" rows="3"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <input type="submit" value="Enviar" class="btn btn-primary"/>
+                        <input type="submit" value="Enviar" name="Enviar" class="btn btn-primary"/>
                     </div>
                 </form>
             </div>
